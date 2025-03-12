aa195bcaeb284345864ea93ebe09099e46511330
irinapryduvys <irinapryduvys@gmail.com>
2025-03-12 18:12:39 +0200

    Update SESHelper.cs

--- src/Clouds.Net.AWS/Helpers/SESHelper.cs
+++ src/Clouds.Net.AWS/Helpers/SESHelper.cs
@@ -37,11 +37,22 @@ namespace Clouds.Net.AWS.Helpers
             _sourceMail = sourceMail;
         }
 
+        /// <summary>
+        /// Asynchronously sends an email using the details specified in the <see cref="SESRequestDto"/> object.
+        /// </summary>
+        /// <param name="obj">The email request object containing recipient, subject, and body details.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is <c>true</c> if the email is sent successfully; otherwise, <c>false</c>.</returns>
         public async Task<bool> SendMail(SESRequestDto obj)
         {
             return await SendMail(obj, _sourceMail);
         }
 
+        /// <summary>
+        /// Asynchronously sends an email using the details specified in the <see cref="SESRequestDto"/> object and allows specifying a source email address.
+        /// </summary>
+        /// <param name="obj">The email request object containing recipient, subject, and body details.</param>
+        /// <param name="sourceMail">The sender's email address.</param>
+        /// <returns>A task that represents the asynchronous operation. The task result is <c>true</c> if the email is sent successfully; otherwise, <c>false</c>.</returns>
         public async Task<bool> SendMail(SESRequestDto obj, string sourceMail)
         {
             try
